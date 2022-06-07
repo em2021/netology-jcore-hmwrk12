c9be7717575420c3ac61853b2d715ada2b58f088
Еgor Moltchanov <egor_m@icloud.com>
2022-06-07 23:51:04 +0300

    First commit

---
+++ Worker.java
@@ -0,0 +1,29 @@
+public class Worker {
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 1; i <= 100; i++) {
+            callback.onDone("Task " + i + " is done");
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is NOT done");
+            }
+        }
+    }
+}
